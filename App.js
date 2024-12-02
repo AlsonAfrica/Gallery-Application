1359c4a572f3394a8233f87802a5da418b5f5a7e
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-12-02 09:44:46 +0200

    Feature: flatlist of images

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View ,Image, FlatList } from 'react-native';
+import { photos } from './data';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      {/* FLAT LIST DATA RENDERS AN ARRAY DATA={} AND RENDERS ITEMS WITHIN THAT DATA */}
+      <FlatList
+      data={photos}
+      contentContainerStyle={{gap:5}}
+      columnWrapperStyle={{gap:4}}
+      numColumns={4}
+      renderItem={({item})=>(<Image source={item.image} style={{width: `${100/4}%`,height:100,}}/>)}
+      />
+      
     </View>
   );
 }
@@ -14,8 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
